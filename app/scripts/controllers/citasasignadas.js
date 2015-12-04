6945dfb9c860fb17cd0c41900115b6d3ecc9a33d
Lucero Vivas <lucerovivas@gmail.com>
2015-12-04 13:51:41 -0500

    Base Front

---
+++ app/scripts/controllers/citasasignadas.js
@@ -0,0 +1,40 @@
+'use strict';
+/**
+ * @ngdoc function
+ * @name sbAdminApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sbAdminApp
+ */
+angular.module('sbAdminApp')
+  .controller('CitasasignadasCtrl', function($scope,$position) {
+  	$scope.prueba="Esto se carga desde el controlador"
+  	$scope.citas = [
+  		{
+  			tipo:"Medicina General",
+  			fecha_hora:"27-11-2015 04:15",
+  			medico: "Juan Perez"
+  		},{
+  			tipo:"Valoración Oral",
+  			fecha_hora:"30-11-2015 11:00",
+  			medico: "Maria Maria"
+  		},{
+  			tipo:"Terapia Ocupacional",
+  			fecha_hora:"27-11-2015 04:15",
+  			medico: "Lastenia Rojas"
+  		},
+  	]
+
+  	$scope.citaSeleccionada = {
+  			tipo:"",
+  			fecha_hora:"",
+  			medico: ""
+  		}
+  	$scope.detalleCita = function(cita){
+		//alert(JSON.stringify(cita));
+  		$scope.citaSeleccionada = cita
+  	}
+  	$scope.generarTurno = function(){
+  		alert(JSON.stringify($scope.citaSeleccionada))
+  	}
+  });
